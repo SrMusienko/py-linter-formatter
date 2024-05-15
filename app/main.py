c0188e4e8bd42d5a02d2ffc2f53c7c74fca873af
Sergii Musiienko <vpnmus@ukr.net>
2024-05-15 18:47:08 +0300

    fixing fix

--- app/main.py
+++ app/main.py
@@ -14,13 +14,13 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(err_point) for err_point in errors],
         "path": file_path,
-        "status": "failed" if bool(errors) else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
 
     return [
-        format_single_linter_file(key, linter_report[key])
-        for key in linter_report.keys()
+        format_single_linter_file(key, val)
+        for key, val in linter_report.items()
     ]
